34acc6b58ebc413614bc807a5ebf29e2a84e35d9
szymondobrodziej <64963558+szymondobrodziej@users.noreply.github.com>
2021-03-01 18:48:31 +0100

    Initial commit

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,16 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+//Dzień dobry WSEI
+/*
+    Tu znajduje się komentarz wielolinijkowy.
+ */
+
+
+
+ class Main {
+    /** Tu znajduje się komentarz javadoc */
+    public static void main(String[] args) {
+        System.out.println("Hello Java world\n");
+
+    }
+}
